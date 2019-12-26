4d840789f5c9782686ce6e912eecfd0dbb78fc6f
wesaka <kalatai.alberti@gmail.com>
2019-12-25 22:22:01 -0300

    Added a new sign-in page with custom form input and a new custom button

---
+++ src/pages/sign/sign.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./sign.styles.scss"
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignPage = () => (
+    <div className='sign'><SignIn/></div>
+);
+
+export default SignPage;
