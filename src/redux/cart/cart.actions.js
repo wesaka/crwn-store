3c41f1ebbe8c4ecdb2a488eed0a1b0edeafae48c
wesaka <kalatai.alberti@gmail.com>
2019-12-27 11:43:50 -0300

    Updated shop data with data that wouldn't cause conflicts, also updated cart, now grouping same products under cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,4 +4,14 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
+
+// Actions always have:
+// Type, that is what the user is trying to do
+// Payload is what the value of the action
+
 
