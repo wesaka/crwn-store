d75f2649357f4a7af228a272be1e61d2badb416a
wesaka <kalatai.alberti@gmail.com>
2019-12-27 12:24:11 -0300

    Updated the application with reselect. Replaced the cart selector for memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,7 @@ import './cart-dropdown.styles.scss'
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
@@ -18,9 +19,10 @@ const CartDropdown = ({cartItems}) => (
 
 // Map dispatch to props is used to turn things into state
 // Map state to props is used to turn things into props, to read states
-const mapStateToProps = ({cart: {cartItems}}) => ({
+const mapStateToProps = state => ({
     // This name right here is what is used in props
-    cartItems
+    // We are also using reselect selectors here, so the whole component doesn't need to be re-rendered every time something unrelated to the cart changes in the application
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { connect } from 'react-redux'
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import './cart-icon.styles.scss'
 
 import { ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 
 // Remember to pass the action as props for the component
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -20,4 +21,11 @@ const mapDispatchToProps = dispatch => ({
 
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    // Reduce is kinda like a nested foreach loop to read and sum all the cartItems quantities
+    // Again, remember to pass this value as props
+    // Here, we are using a selector (Memoized selector provided by reselect), and we have to pass the whole state for it
+    itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
