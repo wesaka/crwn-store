8f447d2ac9d897688383d7d0d5ae99a62953a8d0
wesaka <kalatai.alberti@gmail.com>
2020-01-29 17:58:50 -0300

    Updated App and Shop components to also use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {Switch, Route, Redirect} from 'react-router-dom'
 
@@ -16,35 +16,24 @@ import { checkUserSession } from "./redux/user/user.actions";
 import {createStructuredSelector} from "reselect";
 
 
-class App extends React.Component {
-    unsubscribeFromAuth = null;
+const App = ({ checkUserSession, currentUser }) => {
+    useEffect(() => {
+        checkUserSession()
+    }, [checkUserSession]);
 
-    componentDidMount() {
-        const { checkUserSession } = this.props;
-        checkUserSession();
-    }
-
-    // But when the component unmount, we tell the observable  to stop listening to changes from firebase
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
-
-    render() {
-        return (
-            <div>
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/shop' component={ShopPage}/>
-                    <Route exact path='/checkout' component={CheckoutPage}/>
-                    <Route exact path='/sign'
-                           render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignPage/>)}/>
-                </Switch>
-            </div>
-        );
-    }
-
-}
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+                <Route exact path='/checkout' component={CheckoutPage}/>
+                <Route exact path='/sign'
+                       render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignPage/>)}/>
+            </Switch>
+        </div>
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { useEffect } from "react";
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -8,30 +8,25 @@ import CollectionsOverviewContainer from "../../components/collections-overview/
 import CollectionPageContainer from "../collection/collection.container";
 // We have access to the match object here because we are using a route component in App.js to arrive here
 // Match.path allows us to route to a new page without having to worry about the url so far
-class ShopPage extends Component {
+const ShopPage = ({ fetchCollectionsStart, match }) => {
     // Remember to access the redux function that is being passed on mapDispatchToProps
     // The sole purpose of the component is to initialize the start to fetching data and render the components
-    componentDidMount() {
-        const { fetchCollectionsStart } = this.props;
+    useEffect( () => {
         fetchCollectionsStart();
-    }
+    }, [fetchCollectionsStart]);
 
     // And here to access the isCollectionFetching prop being passed on mapStateToProps
-    render() {
-        const { match } = this.props;
+    return (
+        // Here we swapped the WithSpinner component for the container, as all variables are handled there
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`}
+                   component={CollectionsOverviewContainer}/>
 
-        return (
-            // Here we swapped the WithSpinner component for the container, as all variables are handled there
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`}
-                       component={CollectionsOverviewContainer}/>
-
-                <Route path={`${match.path}/:collectionId`}
-                       component={CollectionPageContainer}/>
-            </div>
-        );
-    }
-}
+            <Route path={`${match.path}/:collectionId`}
+                   component={CollectionPageContainer}/>
+        </div>
+    );
+};
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
